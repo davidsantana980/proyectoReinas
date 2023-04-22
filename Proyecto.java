cfb46458d96f9888b2bca5ef85b4b62061dfdf89
davidsantana980 <108551512+davidsantana980@users.noreply.github.com>
2023-04-21 21:40:19 -0400

    nuevo menu joptionpane, version preliminar

--- Proyecto.java
+++ Proyecto.java
@@ -1,40 +1,33 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 final class Proyecto {
-    private static Scanner lee = new Scanner(System.in); 
-
-    private static int numeroEntero(){
+    private static int numeroEntero() {
         int numero = 0;
         boolean deNuevo = true;
 
-        while(deNuevo){
-            if(lee.hasNextInt()){
-                numero = lee.nextInt();
+        while (deNuevo) {
+            try {
+                String input = JOptionPane.showInputDialog(null, "Introduce un número entero:");
+                numero = Integer.parseInt(input);
                 deNuevo = false;
-                break;
-            }
-            if(!(lee.hasNextInt())){
-                System.out.println("\nNo introduciste un numero! Introduce un numero entero:");
-                lee.next();
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "No introduciste un número! Introduce un número entero:");
             }
         }
         return numero;
     }
 
-    private static double numeroDoble(){
+    private static double numeroDoble() {
         double numero = 0;
         boolean deNuevo = true;
 
-        while(deNuevo){
-            if(lee.hasNextInt()){
-                numero = lee.nextDouble();
+        while (deNuevo) {
+            try {
+                String input = JOptionPane.showInputDialog(null, "Introduce un número:");
+                numero = Double.parseDouble(input);
                 deNuevo = false;
-                break;
-            }
-
-            if(!(lee.hasNextInt())){
-                System.out.println("\nNo introduciste un numero! Introduce un numero:");
-                lee.next();
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "No introduciste un número! Introduce un número:");
             }
         }
         return numero;
@@ -49,39 +42,41 @@ final class Proyecto {
             System.out.println(primoOCompuesto);
         }catch(Error e){
             System.out.println(e.getMessage());
-        }finally{
+        }finally{ System.out.println("Aun quedan mas opciones para usar en el programa! ");
             menu();
         }
     }
 
 
     private static void ejercicioMCM_MCD(){
-
         System.out.println("Introduce tres numeros:\n");
-
         int primerTermino = numeroEntero();;
         int segundoTermino = numeroEntero();
         int tercerTermino = numeroEntero();
-
         Numeros numeros = new Numeros(primerTermino, segundoTermino, tercerTermino);
 
+        try{
         int mcd = numeros.mcd();
         System.out.println("\nEl maximo comun divisor de " + primerTermino + ", "+segundoTermino+" y " + tercerTermino + " es " + mcd);
 
         int mcm = numeros.mcm();
         System.out.println("\nEl minimo comun multiplo de " + primerTermino + ", "+segundoTermino+" y " + tercerTermino + " es " + mcm);
-
-        menu();
+        }catch(Error e){
+            System.out.println(e.getMessage());
+        }finally{ System.out.println("Aun quedan mas opciones para usar en el programa! ");
+            menu();
+        }
     }
 
     private static void ejercicioDesglose(){
+        System.out.println("Ingrese el valor del billete");
+        Numero billete = new Numero(numeroEntero());
+
         try{
-            System.out.println("Ingrese el valor del billete");
-            Numero billete = new Numero(numeroEntero());
-            billete.desgloseBillete();      
+          billete.desgloseBillete();      
         }catch(Error e){
             System.out.println(e.getMessage());
-        }finally{
+        }finally{ System.out.println("Aun quedan mas opciones para usar en el programa! ");
             menu();
         }
     }
@@ -95,13 +90,12 @@ final class Proyecto {
             numero.factorial();
         }catch(Error e){
             System.out.println(e.getMessage());
-        }finally{
+        }finally{ System.out.println("Aun quedan mas opciones para usar en el programa! ");
             menu();
         }
     }
 
     private static void ejercicioEcuacion(){
-
         System.out.println("Introduzca la variable a");
         double a = numeroDoble();
         System.out.println("Introduzca la variable b");
@@ -111,137 +105,143 @@ final class Proyecto {
 
         Numeros numeros = new Numeros(a, b, c);
 
+        try{
         numeros.ecuacionCuadratica();
-
-        menu();
+        }catch(Error e){
+            System.out.println(e.getMessage());
+        }finally{ System.out.println("Aun quedan mas opciones para usar en el programa! ");
+            menu();
+        }
     }
 
     private static void ejercicioPilas(){
-        Pila<Integer> pila = new Pila<>();
+            Pila<Integer> pila = new Pila<>();
 
-        String mensaje = "";
+            String mensaje = "";
 
-        boolean deNuevo = true;
-        while(deNuevo){
-            
-            System.out.print("\nEscribe uno de los comandos: \'estado\', \'llenar\', \'cantidad\', \'top\', \'elimina\', \'contenido\', \'modifica\' o \'salir\', para cerrar el menu pilas: ");
+            boolean deNuevo = true;
+            while(deNuevo){
+   
+                String opcion = JOptionPane.showInputDialog("Escribe uno de los comandos: \'estado\', \'llenar\', \'cantidad\', \'top\', \'elimina\', \'contenido\', \'modifica\' o \'salir\', para cerrar el menu pilas: ");
 
-            switch(lee.next()){
-                case "contenido": 
+                switch(opcion){
+                    case "contenido": 
 
-                    if(!pila.vacia()){
-                        System.out.println("\nEl contenido de la pila es: \n");
-                        pila.muestraTodo();
-                    }else{
-                        System.out.println("\nNo hay contenido que ver");
-                    }
+                        if(!pila.vacia()){
+                            System.out.println("\nEl contenido de la pila es: \n");
+                            pila.muestraTodo();
+                        }else{
+                            System.out.println("\nNo hay contenido que ver");
+                        }
 
-                    break;
-                case "estado": 
-
-                    String vacia = pila.vacia() ? "esta vacia" : "no esta vacia" ;
-                    System.out.println("\nLa pila "+ vacia);
-
-                    break;
-                case "llenar":
-
-                    System.out.println("\nIntroduce numeros a la pila, o introduce cualquier otro caracter para terminar: ");
-                    while(lee.hasNextInt()){
-                        int numero = lee.nextInt(); 
-                        pila.push(numero);
-                    }
-
-                    if(!(lee.hasNextInt())){
-                        //consume la linea antes de repetir el menu
-                        lee.next();
                         break;
-                    }
+                    case "estado": 
 
-                    break;
-                case "cantidad":
+                        String vacia = pila.vacia() ? "esta vacia" : "no esta vacia" ;
+                        System.out.println("\nLa pila "+ vacia);
 
-                    System.out.println("\nHay "+ pila.cantidadDeElementos() + " elementos en la lista.");
+                        break;
+                    case "llenar":
 
-                    break;
-                case "top":
+                        mensaje = "Introduce numeros a la pila, o introduce cualquier otro caracter para terminar: ";
+                        boolean leer = true;
+                        while(leer){
+                              try {
+                                   String input = JOptionPane.showInputDialog(null, mensaje);
+                                   int numero = Integer.parseInt(input); 
+                                   pila.push(numero);
+                              } catch (NumberFormatException e) {     
+                              leer = false;
+                       }
+                }
 
-                    mensaje = "\nEl ultimo elemento de la lista es nulo";
-                    if(!pila.vacia()){
-                        mensaje = "\nEl pico de la pila es " +  pila.top();
-                    }     
-                    System.out.println(mensaje);
+                        break;
+                   case "cantidad":
 
-                    break;
-                case "pop":
+                       mensaje = "Hay "+ pila.cantidadDeElementos() + " elementos en la lista.";
+                       JOptionPane.showMessageDialog(null, mensaje);
 
-                    mensaje = "\nNo hay elementos que eliminar";
+                        break;
+                    case "top":
 
-                    if(!pila.vacia()){
-                        mensaje = "\nEl numero sacado es: " + pila.pop();
-                    }                        
+                       mensaje = "\nEl ultimo elemento de la lista es nulo";
+                       if(!pila.vacia()){
+                            mensaje = "El pico de la pila es " +  pila.top();
+                       }     
+                       JOptionPane.showMessageDialog(null, mensaje);
+
+                       break;
+                    case "pop":
+
+                            mensaje = "No hay elementos que eliminar";
+
+                            if(!pila.vacia()){
+                                mensaje = "\nEl numero sacado es: " + pila.pop();
+                            }                        
                     
-                    System.out.println(mensaje);
+                        System.out.println(mensaje);
 
-                    break;
-                case "modifica":
-                    mensaje = "\nLa pila esta vacia";
+                        break;
+                    case "modifica":
+                        mensaje = "\nLa pila esta vacia";
 
-                    if(!pila.vacia()){
-                        System.out.println("Escribe el indice del numero que quieres cambiar: ");
-                        int index = numeroEntero();
+                        if(!pila.vacia()){
+                            System.out.println("Escribe el indice del numero que quieres cambiar: ");
+                            int index = numeroEntero();
 
-                        while(index >= pila.cantidadDeElementos() || index < 0){
-                            System.out.println("Ese indice es invalido! Intenta de nuevo:");
-                            index = numeroEntero();
-                        }
+                            while(index >= pila.cantidadDeElementos() || index < 0){
+                                System.out.println("Ese indice es invalido! Intenta de nuevo:");
+                                index = numeroEntero();
+                            }
 
-                        System.out.println("Escribe el numero por el que quieres sustituir el elemento en el indice " + index + " (el valor actual en ese indice es "+ pila.mira(index)+"): ");
-                        int nuevoNumero = numeroEntero();    
+                            System.out.println("Escribe el numero por el que quieres sustituir el elemento en el indice " + index + " (el valor actual en ese indice es "+ pila.mira(index)+"): ");
+                            int nuevoNumero = numeroEntero();    
 
-                        pila.modifica(index, nuevoNumero);
+                            pila.modifica(index, nuevoNumero);
 
-                        mensaje = "\nElemento correctamente modificado. Nuevo valor: " + nuevoNumero;
-                    }     
+                            mensaje = "\nElemento correctamente modificado. Nuevo valor: " + nuevoNumero;
+                        }     
 
-                    System.out.println(mensaje);    
-                    break;
-                case "elimina":
-                    mensaje = "\nNo hay elementos que eliminar";
+                        System.out.println(mensaje);    
+                        break;
+                    case "elimina":
+                        mensaje = "\nNo hay elementos que eliminar";
 
-                    if(!pila.vacia()){
-                        System.out.println("\nEscribe el indice del numero que quieres cambiar: ");
-                        int index = numeroEntero();
+                        if(!pila.vacia()){
+                            System.out.println("\nEscribe el indice del numero que quieres cambiar: ");
+                            int index = numeroEntero();
 
-                        while(index >= pila.cantidadDeElementos() || index < 0){
-                            System.out.println("Ese indice es invalido! Intenta de nuevo:");
-                            index = numeroEntero();
-                        }
+                            while(index >= pila.cantidadDeElementos() || index < 0){
+                                System.out.println("Ese indice es invalido! Intenta de nuevo:");
+                                index = numeroEntero();
+                            }
 
-                        pila.pop(index);
-                        mensaje = "\nEl numero ha sido eliminado.";
-                    }                        
+                            pila.pop(index);
+                            mensaje = "\nEl numero ha sido eliminado.";
+                        }                        
                     
-                    System.out.println(mensaje);
-                    break;
-                case "salir":
+                        System.out.println(mensaje);
+                        break;
+                    case "salir":
 
-                    System.out.println("\nHasta pronto! ");
-                    deNuevo = false;
+                        System.out.println("\nHasta pronto! ");
+                        deNuevo = false;
 
-                    break;
+                        break;
+                }
             }
-        }
 
-        menu();
+            menu();
     }
 
     private static void menu(){
-        System.out.print("\nBienvenido de vuelta al menu principal! Escribe uno de los comandos: \'primos\', \'factorial\', \'comunes\', \'pilas\', \'desglose\', \'ecuacion\' o \'salir\' para cerrar el programa: ");
-        
+        JOptionPane.showMessageDialog(null, "Bienvenido de vuelta al menu principal!");
+
         try{
-            boolean deNuevo = true;
-            while(deNuevo){
-                switch(lee.next()){
+          boolean deNuevo = true;
+          while (deNuevo) {
+              String input = JOptionPane.showInputDialog(null, "Escribe uno de los comandos: 'primos', 'factorial', 'comunes', 'pilas', 'desglose', 'ecuacion' o 'salir' para cerrar el programa:");
+              switch (input) {
                     case "primos":
                         ejercicioPrimos();
                         break;
@@ -261,46 +261,40 @@ final class Proyecto {
                         ejercicioEcuacion();
                         break;
                     case "salir":
-                        System.out.println("\nHasta pronto! \n");
+                        JOptionPane.showMessageDialog(null, "Hasta pronto! ");
                         deNuevo = false;
                         break;
                     default: 
-                        System.out.print("\nComando invalido! Escribe bien uno de los comandos indicados, o \'salir\', si quieres terminar: ");
+                        JOptionPane.showMessageDialog(null, "Comando invalido! Escribe bien uno de los comandos indicados, o \'salir\', si quieres terminar: ");
                         break;
                 }
             }
         }finally{
-            lee.close();
             System.exit(0);
         }
     }
     
     public static void main(String[] args) {
-        String usuario = "";
-        String clave = "";
-        System.out.println("Bienvenido Usuario, introduzca su usuario y clave:");
+        JOptionPane.showMessageDialog(null, "Bienvenido Usuario, introduzca su usuario y clave:");
         
         try {
             for(int intentos = 0; intentos < 3; intentos++){
-                System.out.println("Usuario:");
-                usuario = lee.nextLine();
-                System.out.println("clave:");
-                clave = lee.nextLine();
+                String usuario = JOptionPane.showInputDialog("Usuario:");
+                String clave = JOptionPane.showInputDialog("Clave:");
                 
                 if (usuario.isEmpty() || clave.isEmpty()) {
-                    System.out.println("No se han ingresado datos, por favor vuelva a intentarlo");
+                    JOptionPane.showMessageDialog(null, "No se han ingresado datos, por favor vuelva a intentarlo");
                 } else {
                     if (usuario.equals("papu")  && clave.equals("mortadela")) {
                         menu();
                         break;
                     } else {
-                        System.out.println("Lo siento, parece ser que los datos introducidos no son correctos");
+                         JOptionPane.showMessageDialog(null, "Lo siento, parece ser que los datos introducidos no son correctos");
                     }
                     if (intentos == 2) throw new Exception("Ha superado el numero maximo de intentos permitidos");
                 }
             } 
         }catch(Exception e){
-            lee.close();
             System.out.println(e.getMessage());
             System.exit(0);
         }
